0402b933a537085ce59ea2f27436157bce139241
unknown <samantha.c.egge@gmail.com>
2019-07-19 08:25:47 -0700

    Set up App.js with Links/Routes. Dummy Login/Home pages sucessfully rendering.

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,13 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
+import { Route, Link } from 'react-router-dom';
 
 import './App.scss';
+import Login from './components/Login/Login'
+import Home from './components/Home/Home'
 
 function App() {
+  const [token, setToken] = useState(null)
+
+  useEffect(() => {
+    setToken(localStorage.getItem('token'))
+    console.log('testing for infinite loop')
+  }, [token]);
+
+  console.log('App CL token: ', token);
+
   return (
     <div className="app">
       <header className="app-header">
-        React App
+        <h1>React App</h1>
+        <div className='app-nav'>
+          <Link className='app-link' to='/'>
+            Home
+          </Link>
+          {token === null ? (
+            <Link className='app-link' to='/login'>
+              Login
+            </Link>
+          ) : (
+            <Link to='/login'>
+              <button className='btn' onClick={() => {
+                localStorage.removeItem('token')
+                setToken()}} >
+                Logout
+              </button>
+            </Link>
+          )}
+        </div>
       </header>
+      <div className='app-body'>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+      </div>
     </div>
   );
 }
