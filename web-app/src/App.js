b82d98f8aa5fd6d8f2ddcd2e13dab4cec789f8d0
unknown <samantha.c.egge@gmail.com>
2019-07-19 08:12:03 -0700

    Installed npm to BE. Installed CRA, axios, formik, and sass. Set up initial files/structure.

---
+++ web-app/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        React App
+      </header>
+    </div>
+  );
+}
+
+export default App;
