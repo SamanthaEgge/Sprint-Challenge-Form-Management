9f7544cba7d16dcf03899d3993d7a411c764ee16
unknown <samantha.c.egge@gmail.com>
2019-07-19 08:59:04 -0700

    Installed semanitic ui. Developed form for login/registration. Created buttons to toggle registration vs login. Not tested yet.

--- web-app/src/components/Login/Login.js
+++ web-app/src/components/Login/Login.js
@@ -1,11 +1,109 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import axios from 'axios'
+import { Redirect } from 'react-router-dom'
+import { Button } from 'semantic-ui-react'
 
-function Login() {
+import './Login.scss'
+
+function Login({ touched, errors}) {
+  const token = localStorage.getItem('token');
+  const [login, setLogin] = useState('login')
+
+  if (token) {
+    return <Redirect to='/' />
+  }
+
+  const toggleLogin = (event) => {
+    event.preventDefault()
+    setLogin('login')
+    console.log(login)
+  }
+
+  const toggleRegistration = (event) => {
+    event.preventDefault()
+    setLogin('registration')
+    console.log(login)
+  }
+
   return(
-    <div>
-      Login page
+    <>
+    <div className='login-header'>
+      <h2>Login</h2>
     </div>
+    <div className='registration-check'>
+      <Button.Group size='large'>
+        <Button onClick={toggleLogin}>Login</Button>
+        <Button.Or />
+        <Button onClick={toggleRegistration}>Register</Button>
+      </Button.Group>
+    </div>
+    <Form className='login-form'>
+      <div className='form-group'>
+        <label className='label'>Username</label>
+        <Field
+          className='input'
+          name='username'
+          type='username'
+          autoComplete='off'
+          />
+        <p>{touched.username && errors.username}</p>
+      </div>
+      <div className='form-group'>
+        <label className='label'>Password</label>
+        <Field
+          className='input'
+          name='password'
+          type='password'
+          autoComplete='off'
+          />
+        <p>{touched.password && errors.password}</p>
+      </div>
+    </Form>
+    </>
   )
 }
 
-export default Login
+export default withFormik({
+  mapPropsToValues() {
+    return {
+      username: '',
+      password: ''
+    };
+  },
+  validationSchema: Yup.object().shape({
+    username: Yup.string()
+      .required()
+      .min(3),
+    password: Yup.string()
+      .required()
+      .min(6)
+  }),
+  handleSubmit(values, formikBag) {
+    {formikBag.login === 'login' ? 
+    axios
+      .post('http://localhost:6000/api/login', values)
+      .then((response) => {
+        localStorage.setItem('token', response.data.payload);
+        console.log('checking for token data:', response.data.payload)
+        formikBag.props.history.push('/');
+        formikBag.props.setToken(response.data.payload)
+      })
+      .catch((e) => {
+        console.log(e.response.data);
+      }) :
+    axios
+      .post('http://localhost:6000/api/register', values)
+      .then((response) => {
+        localStorage.setItem('token', response.data.payload);
+        console.log('checking for token data:', response.data.payload)
+        formikBag.props.history.push('/');
+        formikBag.props.setToken(response.data.payload)
+      })
+      .catch((e) => {
+        console.log(e.response.data);
+      })
+  }
+}
+})(Login);
